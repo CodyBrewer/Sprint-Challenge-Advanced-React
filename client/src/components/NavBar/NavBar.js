736c257ff2cb9b9ddb0b531a69919e63fa13e4db
CodyBrewer <brewer.cody.m@gmail.com>
2020-01-25 10:55:55 -0700

    NavBar and styling set up

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useDarkMode } from "../../hooks/useDarkMode";
 
 const NavBar = () => {
-  return <div></div>;
+  const [darkMode, setDarkMode] = useDarkMode();
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <nav className="navbar">
+      <h1>Women's Soccer Players Searches from the World Cup</h1>
+      <div className="dark-mode__toggle">
+        <div
+          onClick={toggleMode}
+          className={darkMode ? "toggle toggled" : "toggle"}
+        />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
